a2554b2aa09658897b505cf31417728479522253
YagiGo <jeremywu1995@gmail.com>
2018-04-27 14:55:18 +0900

    Basic index and js files added

---
+++ templates/input_handle.js
@@ -0,0 +1,23 @@
+/**
+ * Created by zhaoxin on 18/04/27.
+ */
+function readJSONFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', file, false);
+    rawFile.onreadystatechange = function() {
+        if(rawFile.readyState == 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                //convert to JSON object
+                var JSONText = JSON.parse(allText);
+            }
+        }
+    }
+    console.log(JSONText);
+
+}
+window.onload = document.getElementById('submitURL').addEventListener('click', function() {
+    var inputURL = document.getElementById('profilingURL').value;
+    console.log(inputURL);
+    // alert(inputURL);
+});
