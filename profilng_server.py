6cef0acdc8f3ba7275388fe07e4e8f5ac1f4c151
YagiGo <jeremywu1995@gmail.com>
2018-04-27 17:51:53 +0900

    Basic index and js files added

---
+++ profilng_server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', name='joe')
+@app.route('/receiver', methods=['POST'])
+def worker():
+    return request
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
