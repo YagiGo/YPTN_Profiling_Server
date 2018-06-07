c033780063ae5bd62993893bcf71f0e05d461721
YagiGo <jeremywu1995@gmail.com>
2018-06-07 17:38:39 +0900

    add restful api

--- api.py
+++ api.py
@@ -6,27 +6,32 @@ from WProfX import trace_parser as profiler
 import _thread
 import os
 
+
 app = Flask(__name__)
 api = Api(app)
 urls_to_be_profiled = {}
 
 # Handle the urls the applications put into
 class DealingWithUrls(Resource):
-    def put_url_to_be_profiled(self, url):
-        urls_to_be_profiled[uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)] = url
-        return "URL {} was accepted! Will be profiled".format(url)
+    def put(self, url):
+        urls_to_be_profiled[str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID))] = request.form['data']
+        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)): urls_to_be_profiled[str(uuid.uuid3
+        (name=url, namespace=uuid.NAMESPACE_OID))]}
+    def get(self, url):
+        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)): urls_to_be_profiled[str(uuid.uuid3
+                    (name=url, namespace=uuid.NAMESPACE_OID))]}
 
 
 # Do the profiling
 class ProfilingSites(Resource):
     def profiling_process(self,url):
-        requested_site = urls_to_be_profiled[uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)]
+        requested_site = urls_to_be_profiled[str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID))]
         fetch.traceWithInput(requested_site)
         profiler.analyzeTrace(requested_site.split('/')[-1])
         response = {'status':200, 'site':requested_site}
         return jsonify(response)
 
-    def get_profiled(self, url):
+    def put(self, url):
         # Start a new thread to process
         try:
             _thread.start_new_thread(self.profiling_process, (url))
@@ -36,13 +41,13 @@ class ProfilingSites(Resource):
 
 class ShowingResults(Resource):
     def reading_profiled_result(self, url):
-        requested_site = urls_to_be_profiled[uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)]
+        requested_site = urls_to_be_profiled[str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID))]
         json_file_path = ''.join([os.path.normpath(os.path.dirname(os.path.realpath(__file__))), '/WProfX', '/graphs/',
                         '0_', requested_site.split('/')[-1].replace(" ", ""), '.json'])
         with open(json_file_path, 'r') as f:
             return jsonify(f.read())
 
-    def get_profiled_data_json(self, url):
+    def get(self, url):
         # Start a new thread to process
         try:
             _thread.start_new_thread(self.reading_profiled_result, (url))
@@ -51,7 +56,7 @@ class ShowingResults(Resource):
 
 
 # adding profiling urls
-api.add_resource(DealingWithUrls, '/input_urls/<string:url>')
+api.add_resource(DealingWithUrls, '/input_urls/<string:url>', '/')
 api.add_resource(ProfilingSites, '/profiling/<string:url>')
 api.add_resource(ShowingResults, '/showing_result/<string:url>')
 
