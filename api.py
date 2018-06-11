ee37daf1c28fa6dba3fbe9ba7c1230522e11f4e0
YagiGo <jeremywu1995@gmail.com>
2018-06-11 13:21:56 +0900

    implementing RESTful API for YPTN Profiler

--- api.py
+++ api.py
@@ -1,24 +1,37 @@
 from flask import Flask, request, jsonify
 from flask.ext.restful import Resource, Api
+# from flask.ext.pymongo import PyMongo
+from db import config
 import uuid
 from WProfX import main as fetch
 from WProfX import trace_parser as profiler
 import _thread
 import os
+from urllib.parse import unquote_plus
 
 
 app = Flask(__name__)
+app.config.from_object(config)
+# db_instance = PyMongo(app)
 api = Api(app)
 urls_to_be_profiled = {}
 
 # Handle the urls the applications put into
+class DealingWithUsers(Resource):
+    def put(self):
+        pass
+
+    def get(self):
+        pass
+
+
 class DealingWithUrls(Resource):
     def put(self, url):
         urls_to_be_profiled[str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID))] = request.form['data']
-        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)): urls_to_be_profiled[str(uuid.uuid3
+        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)):urls_to_be_profiled[str(uuid.uuid3
         (name=url, namespace=uuid.NAMESPACE_OID))]}
     def get(self, url):
-        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)): urls_to_be_profiled[str(uuid.uuid3
+        return {str(uuid.uuid3(name=url, namespace=uuid.NAMESPACE_OID)):urls_to_be_profiled[str(uuid.uuid3
                     (name=url, namespace=uuid.NAMESPACE_OID))]}
 
 
@@ -29,14 +42,17 @@ class ProfilingSites(Resource):
         fetch.traceWithInput(requested_site)
         profiler.analyzeTrace(requested_site.split('/')[-1])
         response = {'status':200, 'site':requested_site}
-        return jsonify(response)
+        return 1
 
     def put(self, url):
         # Start a new thread to process
         try:
-            _thread.start_new_thread(self.profiling_process, (url))
+            self.profiling_process(url)
+            # _thread.start_new_thread(self.profiling_process, (url))
+            return 1
         except Exception as e:
             return jsonify({'status':400, 'info':str(e)})
+
 
 
 class ShowingResults(Resource):
